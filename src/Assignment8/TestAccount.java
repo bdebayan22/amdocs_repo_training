ec0ebad00c96c0e22ed428f06561621e55de2594
bdebayan22 <107911951+bdebayan22@users.noreply.github.com>
2022-09-28 11:02:54 +0530

    Add files via upload

---
+++ src/Assignment8/TestAccount.java
@@ -0,0 +1,19 @@
+package Assignment8;
+
+public class TestAccount {
+    public static void main(String[] args) {
+        Account act=new Account();
+
+        Thread1 t1=new Thread1(act,500);
+        Thread1 t2=new Thread1(act,650);
+        Thread1 t3=new Thread1(act,900);
+
+        try{
+            t1.t.join();
+            t2.t.join();
+        }catch (InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
