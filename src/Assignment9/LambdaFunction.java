ccf8eb87a73eae1c63c97ea2cdc784ece9c36933
bdebayan22 <107911951+bdebayan22@users.noreply.github.com>
2022-09-29 13:25:07 +0530

    Add files via upload

---
+++ src/Assignment9/LambdaFunction.java
@@ -0,0 +1,17 @@
+package Assignment9;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LambdaFunction {
+    public static void main(String[] args) {
+        List<Student> stud=new ArrayList<>();
+        stud.add(new Student("Peter","English"));
+        stud.add(new Student("Akash","Physics"));
+        stud.add(new Student("Jay","Computer Science"));
+        stud.add(new Student("Priya","English"));
+       Collections.sort(stud,(Student s1,Student s2)->s1.getName().compareTo(s2.getName()));
+       System.out.println(stud);
+    }
+}
